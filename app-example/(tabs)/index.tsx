c32a1925bcec3da62c36c8845c3a1252bb0ee33f
thienhdbse170185 <thienhdbse170185@fpt.edu.vn>
2024-05-13 01:49:57 +0700

    mod: bun reset-project

--- app-example/(tabs)/index.tsx
+++ app-example/(tabs)/index.tsx
@@ -17,7 +17,7 @@ export default function HomeScreen() {
       }
     >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Welcome, Bảo Thiên!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
