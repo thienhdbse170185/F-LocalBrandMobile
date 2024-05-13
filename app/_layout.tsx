a2886112c5c1ee583aa925aa598673b5e0f89132
thienhdbse170185 <thienhdbse170185@fpt.edu.vn>
2024-05-14 02:04:17 +0700

    add: Montserrat font.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,44 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    MontserratBlack: require("../assets/fonts/Montserrat-Black.ttf"),
+    MontserratBold: require("../assets/fonts/Montserrat-Bold.ttf"),
+    MontserratExtraBold: require("../assets/fonts/Montserrat-ExtraBold.ttf"),
+    MontserratExtraLight: require("../assets/fonts/Montserrat-ExtraLight.ttf"),
+    MontserratLight: require("../assets/fonts/Montserrat-Light.ttf"),
+    MontserratRegular: require("../assets/fonts/Montserrat-Regular.ttf"),
+    MontserratSemiBold: require("../assets/fonts/Montserrat-SemiBold.ttf"),
+    MontserratThin: require("../assets/fonts/Montserrat-Thin.ttf"),
+    MontserratMedium: require("../assets/fonts/Montserrat-Medium.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
+    <Stack
+      initialRouteName="(tabs)"
+      screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor: "white" },
+      }}
+    >
+      <Stack.Screen name="(tabs)" />
+      <Stack.Screen name="(auth)" />
+      <Stack.Screen name="+not-found" />
     </Stack>
   );
 }
