2adedc79408d1d05031454d82c40632439f9b3bf
thienhdbse170185 <thienhdbse170185@fpt.edu.vn>
2024-05-14 14:01:09 +0700

    ref: docs, comment

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import {
+  ActivityIndicator,
   Image,
   KeyboardAvoidingView,
   StyleSheet,
@@ -11,42 +12,66 @@ import {
 import { Controller, useForm } from "react-hook-form";
 import { Colors } from "@/constants/Colors";
 import TextInputIcon from "@/components/form/TextInputIcon";
+import { router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 type FormData = {
   username: string;
   password: string;
 };
 
+/**
+ * Login screen component.
+ *
+ * @returns {JSX.Element} Login screen component.
+ */
 export default function LoginScreen() {
+  // react-hook-form Controller hook for handling form data.
   const {
     control,
     handleSubmit,
     formState: { errors },
   } = useForm<FormData>({
+    // Set default form values.
     defaultValues: {
       username: "",
       password: "",
     },
   });
 
-  const [borderColorUsername, setBorderColorUsername] = useState("gray");
-  const [borderColorPassword, setBorderColorPassword] = useState("gray");
+  // State for handling border color of username input.
+  const [borderColorUsername, setBorderColorUsername] =
+    useState<string>("gray");
+  // State for handling border color of password input.
+  const [borderColorPassword, setBorderColorPassword] =
+    useState<string>("gray");
+  // State for handling loading state.
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
+  /**
+   * Handle form submission.
+   *
+   * @param {FormData} data Form data.
+   */
   const onSubmit = handleSubmit(
     ({ username, password }) => {
-      if (username === null || password === null) {
-        console.error("Null username or password");
+      if (username && password) {
+        setIsLoading(true);
+        setTimeout(() => {
+          setIsLoading(false);
+          alert(username + " " + password);
+        }, 2000);
       } else {
-        alert(username + " " + password);
+        console.error("Null username or password");
       }
     },
     (errors) => {
-      if (errors !== null) {
+      if (errors) {
         console.error("Form submission errors:", errors);
-        if (errors.username !== null) {
+        if (errors.username) {
           setBorderColorUsername("red");
         }
-        if (errors.password !== null) {
+        if (errors.password) {
           setBorderColorPassword("red");
         }
       }
@@ -54,28 +79,37 @@ export default function LoginScreen() {
   );
 
   return (
-    <View style={styles.container}>
-      <Text style={{ fontFamily: "MontserratBold", fontSize: 42 }}>
-        Welcome {"\n"}Back!
-      </Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Welcome {"\n"}Back!</Text>
       <KeyboardAvoidingView style={styles.form}>
         <View style={styles.inputLayout}>
           <Controller
             control={control}
+            // Render method for handling form data.
             render={({ field: { onChange, onBlur, value } }) => (
               <TextInputIcon
+                // Input placeholder text.
                 placeholder="Username"
+                // onChange event handler.
                 onChange={onChange}
+                // onBlur event handler.
                 onBlur={onBlur}
+                // Input value.
                 value={value}
-                borderColorUsername={borderColorUsername}
-                setBorderColorUsername={setBorderColorUsername}
+                // Border color of input.
+                borderColor={borderColorUsername}
+                // Set border color.
+                setBorderColor={setBorderColorUsername}
+                // Name of icon to be displayed on the left side of the input.
                 nameIcon="person"
               />
             )}
+            // Name of the input.
             name="username"
+            // Validation rules for the input.
             rules={{ required: true }}
           />
+          {/* Display error message if there is an error. */}
           {errors.username && (
             <Text style={styles.error}>Username is required.</Text>
           )}
@@ -85,20 +119,32 @@ export default function LoginScreen() {
             control={control}
             render={({ field: { onChange, onBlur, value } }) => (
               <TextInputIcon
+                // Input placeholder text.
                 placeholder="Password"
+                // onChange event handler.
                 onChange={onChange}
+                // onBlur event handler.
                 onBlur={onBlur}
+                // Input value.
                 value={value}
-                borderColorUsername={borderColorPassword}
-                setBorderColorUsername={setBorderColorPassword}
+                // Border color of input.
+                borderColor={borderColorPassword}
+                // Set border color.
+                setBorderColor={setBorderColorPassword}
+                // Determine whether to show the eye icon or not.
                 hasEye={true}
+                // Determine whether the input is secure or not.
                 isSecureTextEntry={true}
+                // Name of icon to be displayed on the left side of the input.
                 nameIcon="lock-closed"
               />
             )}
+            // Name of the input.
             name="password"
+            // Validation rules for the input.
             rules={{ maxLength: 100 }}
           />
+          {/* Display error message if there is an error. */}
           {errors.password && (
             <Text style={styles.error}>Password is required.</Text>
           )}
@@ -107,10 +153,11 @@ export default function LoginScreen() {
               textAlign: "right",
               fontFamily: "MontserratRegular",
               fontSize: 16,
-              color: Colors.light.primary,
+              color: Colors.light.hint,
               marginTop: 10,
               textDecorationLine: "underline",
             }}
+            // onPress event handler.
             onPress={() => {
               // router.navigate("/forgot-password");
               alert("Forgot Password coming soon...");
@@ -120,16 +167,22 @@ export default function LoginScreen() {
           </Text>
         </View>
         <TouchableOpacity
+          // onPress event handler.
           onPress={onSubmit}
           style={styles.button}
           activeOpacity={0.8}
         >
-          <Text style={styles.buttonText}>Login</Text>
+          {isLoading ? (
+            <ActivityIndicator size="large" color="white" />
+          ) : (
+            <Text style={styles.buttonText}>Login</Text>
+          )}
         </TouchableOpacity>
       </KeyboardAvoidingView>
       <View
         style={{
           alignItems: "center",
+          marginTop: 10,
         }}
       >
         <Text
@@ -154,7 +207,13 @@ export default function LoginScreen() {
             <Image source={require("@/assets/images/google.png")} />
           </TouchableOpacity>
         </View>
-        <Text style={{ fontFamily: "MontserratRegular", fontSize: 14 }}>
+        <Text
+          style={{
+            fontFamily: "MontserratRegular",
+            fontSize: 16,
+            marginTop: 15,
+          }}
+        >
           Create An Account?{" "}
           <Text
             style={{
@@ -162,25 +221,26 @@ export default function LoginScreen() {
               color: Colors.light.primary,
               textDecorationLine: "underline",
             }}
+            // onPress event handler.
             onPress={() => {
-              // router.navigate("/register");
-              alert("Register Screen coming soon...");
+              router.navigate("/register");
             }}
           >
             Sign Up
           </Text>
         </Text>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingVertical: 50,
+    paddingVertical: 20,
     paddingHorizontal: 30,
   },
+  title: { fontFamily: "MontserratBold", fontSize: 42 },
   form: {
     marginVertical: 38,
   },
@@ -211,6 +271,6 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     justifyContent: "center",
     alignItems: "center",
-    borderRadius: 48 / 2,
+    borderRadius: 999,
   },
 });
