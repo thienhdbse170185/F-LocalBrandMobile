c5a50f6494c92f38ee04af60acb153d8d64ed815
thienhdbse170185 <thienhdbse170185@fpt.edu.vn>
2024-05-16 11:27:02 +0700

    add: signout demo

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { router } from "expo-router";
 import { useEffect } from "react";
 import { View, Text, SafeAreaView, Pressable } from "react-native";
@@ -8,6 +9,17 @@ export default function Index() {
       <Text>Home Screen</Text>
       <Pressable onPress={() => router.navigate("/login")}>
         <Text>Go to login</Text>
+      </Pressable>
+      <Pressable
+        onPress={() => {
+          AsyncStorage.removeItem("token");
+          router.navigate("(auth)");
+        }}
+        style={{
+          marginTop: 30,
+        }}
+      >
+        <Text>Signout</Text>
       </Pressable>
     </SafeAreaView>
   );
