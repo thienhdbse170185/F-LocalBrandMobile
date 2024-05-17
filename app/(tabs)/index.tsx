9674197488c0d052fcd7d755d1d4f9b3a97c62e3
thienhdbse170185 <thienhdbse170185@fpt.edu.vn>
2024-05-17 13:24:15 +0700

    rm: goto login demo

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,10 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { router } from "expo-router";
-import { useEffect } from "react";
-import { View, Text, SafeAreaView, Pressable } from "react-native";
+import { Text, SafeAreaView, Pressable } from "react-native";
 
 export default function Index() {
   return (
     <SafeAreaView>
-      <Text>Home Screen</Text>
-      <Pressable onPress={() => router.navigate("/login")}>
-        <Text>Go to login</Text>
-      </Pressable>
       <Pressable
         onPress={() => {
           AsyncStorage.removeItem("token");
