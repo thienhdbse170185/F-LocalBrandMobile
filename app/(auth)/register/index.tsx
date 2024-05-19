79b17bdea27bfad951e72553a34479137894fd24
thienhdbse170185 <thienhdbse170185@fpt.edu.vn>
2024-05-20 02:03:30 +0700

    add: validate input has no data

--- app/(auth)/register/index.tsx
+++ app/(auth)/register/index.tsx
@@ -3,15 +3,7 @@ import { Colors } from "@/constants/Colors";
 import { router } from "expo-router";
 import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
-import {
-  View,
-  Text,
-  StyleSheet,
-  KeyboardAvoidingView,
-  TouchableOpacity,
-  Image,
-  Keyboard,
-} from "react-native";
+import { KeyboardAvoidingView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 type FormData = {
@@ -31,13 +23,15 @@ export default function RegisterScreen(): JSX.Element {
     control,
     handleSubmit,
     formState: { errors },
+    setError,
+    clearErrors
   } = useForm<FormData>({
     // Set default form values.
     defaultValues: {
       username: "",
       password: "",
-      confirm: "",
-    },
+      confirm: ""
+    }
   });
 
   // State for handling border color of username input.
@@ -55,26 +49,23 @@ export default function RegisterScreen(): JSX.Element {
    * @param {FormData} data Form data.
    */
   const onSubmit = handleSubmit(
-    ({ username, password }) => {
-      Keyboard.dismiss();
-      if (username !== null && password !== null) {
-        alert(username + " " + password);
-      } else {
-        console.error("Null username or password");
-      }
+    (data) => {
+      console.log(data);
     },
     (errors) => {
-      if (errors !== null) {
-        console.error("Form submission errors:", errors);
-        if (errors.username !== null) {
-          setBorderColorUsername("red");
-        }
-        if (errors.password !== null) {
-          setBorderColorPassword("red");
-        }
+      if (errors.username) {
+        setError("username", { type: "required", message: "Required!" });
+        setBorderColorUsername("red");
       }
-    }
-  );
+      if (errors.password) {
+        setError("password", { type: "required", message: "Required!" });
+        setBorderColorPassword("red");
+      }
+      if (errors.confirm) {
+        setError("confirm", { type: "required", message: "Required!" });
+        setBorderColorConfirm("red");
+      }
+    });
 
   return (
     <SafeAreaView style={styles.container}>
@@ -89,6 +80,10 @@ export default function RegisterScreen(): JSX.Element {
                   placeholder="Username"
                   onChange={onChange}
                   onBlur={onBlur}
+                  onFocus={() => {
+                    clearErrors("username");
+                    setBorderColorUsername("black");
+                  }}
                   value={value}
                   borderColor={borderColorUsername}
                   setBorderColor={setBorderColorUsername}
@@ -101,7 +96,7 @@ export default function RegisterScreen(): JSX.Element {
             // eslint-disable-next-line jsdoc/require-jsdoc
           />
           {errors.username && (
-            <Text style={styles.error}>Username is required.</Text>
+            <Text style={styles.error}>{errors.username.message}</Text>
           )}
         </View>
         <View style={styles.inputLayout}>
@@ -113,6 +108,10 @@ export default function RegisterScreen(): JSX.Element {
                   placeholder="Password"
                   onChange={onChange}
                   onBlur={onBlur}
+                  onFocus={() => {
+                    clearErrors("password");
+                    setBorderColorPassword("black");
+                  }}
                   value={value}
                   borderColor={borderColorPassword}
                   setBorderColor={setBorderColorPassword}
@@ -123,11 +122,11 @@ export default function RegisterScreen(): JSX.Element {
               );
             }}
             name="password"
-            rules={{ maxLength: 100 }}
+            rules={{ required: true }}
             // eslint-disable-next-line jsdoc/require-jsdoc
           />
           {errors.password && (
-            <Text style={styles.error}>Password is required.</Text>
+            <Text style={styles.error}>{errors.password.message}</Text>
           )}
         </View>
         <View style={styles.inputLayout}>
@@ -139,6 +138,10 @@ export default function RegisterScreen(): JSX.Element {
                   placeholder="Confirm Password"
                   onChange={onChange}
                   onBlur={onBlur}
+                  onFocus={() => {
+                    clearErrors("confirm");
+                    setBorderColorConfirm("black");
+                  }}
                   value={value}
                   borderColor={borderColorConfirm}
                   setBorderColor={setBorderColorConfirm}
@@ -149,11 +152,11 @@ export default function RegisterScreen(): JSX.Element {
               );
             }}
             name="confirm"
-            rules={{ maxLength: 100 }}
+            rules={{ required: true }}
             // eslint-disable-next-line jsdoc/require-jsdoc
           />
           {errors.confirm && (
-            <Text style={styles.error}>Confirm is required.</Text>
+            <Text style={styles.error}>{errors.confirm.message}</Text>
           )}
         </View>
         <View>
@@ -161,7 +164,7 @@ export default function RegisterScreen(): JSX.Element {
             style={{
               fontFamily: "MontserratRegular",
               fontSize: 14,
-              color: Colors.light.hint,
+              color: Colors.light.hint
             }}
           >
             By clicking the{" "}
@@ -179,7 +182,7 @@ export default function RegisterScreen(): JSX.Element {
       </KeyboardAvoidingView>
       <View
         style={{
-          alignItems: "center",
+          alignItems: "center"
         }}
       >
         <Text style={{ fontFamily: "MontserratRegular", fontSize: 16 }}>
@@ -188,7 +191,7 @@ export default function RegisterScreen(): JSX.Element {
             style={{
               fontFamily: "MontserratSemiBold",
               color: Colors.light.primary,
-              textDecorationLine: "underline",
+              textDecorationLine: "underline"
             }}
             onPress={() => {
               router.canGoBack() ? router.back() : router.push("/login");
@@ -205,30 +208,30 @@ export default function RegisterScreen(): JSX.Element {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingVertical: 20,
-    paddingHorizontal: 30,
+    paddingVertical: 10,
+    paddingHorizontal: 30
   },
   title: { fontFamily: "MontserratBold", fontSize: 42 },
   form: {
-    marginVertical: 38,
+    marginVertical: 38
   },
   button: {
     backgroundColor: Colors.light.primary,
     height: 60,
     borderRadius: 6,
     justifyContent: "center",
-    marginTop: 30,
+    marginTop: 30
   },
   buttonText: {
     fontSize: 22,
     textAlign: "center",
     color: "white",
-    fontFamily: "MontserratSemiBold",
+    fontFamily: "MontserratSemiBold"
   },
   error: {
-    color: "red",
+    color: "red"
   },
   inputLayout: {
-    height: 90,
-  },
+    height: 90
+  }
 });
